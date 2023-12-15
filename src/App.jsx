faf0ba6702dc0555dd951a04ba27fefc875f4744
Семён Субочев <s.subochev@sz.ld>
2023-12-15 18:32:36 +0300

    добавление+мутация+автосинхрон+кеш запросов

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { useGetGoodsQuery } from './redux';
+import { useGetGoodsQuery, useAddProductMutation } from './redux';
 
 function App() {
+	//строка для хука
+	const [newProduct, setNewProduct] = useState('');
+	const [count, setCount] = useState('');
 	//хук при вызове будет возвращать обьект
 	//произойдёт запрос к серверу за данными в baseUrl + query ('goods')
-	const { data = [], isLoading } = useGetGoodsQuery();
+	//селект будет выбирать и сетать локал стор, а затем каунт после сета закидываем в хук
+	const { data = [], isLoading } = useGetGoodsQuery(count);
+	//хук,на мутацию не требует делать запрос сразу, а только при необходимости
+	//произвольное имя и чет ещё
+	const [addProduct, { isError }] = useAddProductMutation(); //асинхрон
+	//функция для добавления
+	const handleAddProduct = async () => {
+		if (newProduct) {
+			//вот тебе имя товара, которое юзер вбил, отправь запрос мутации в базу данных
+			await addProduct({ name: newProduct }).unwrap(); //для корректной работы всех дополнительных пропсов(в данном случае{isError})
+			setNewProduct(''); //очистка поля ввода
+		}
+	};
+
 	if (isLoading) return <h1>Loading...</h1>;
 	return (
 		<div className="App">
+			<div>
+				<div>
+					<input
+						type="text"
+						value={newProduct}
+						onChange={(e) => setNewProduct(e.target.value)}
+					/>
+					<button onClick={handleAddProduct}>Add Product</button>
+				</div>
+				<select
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+					name=""
+					id="">
+					<option value="''">all</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="5">5</option>
+					<option value="10">10</option>
+				</select>
+			</div>
 			<ul>
 				{data.map((item) => (
 					<li key={item.id}>{item.name}</li>
--- src/redux/goodsApi.js
+++ src/redux/goodsApi.js
@@ -2,6 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const goodsApi = createApi({
 	//отображение в общем стори
 	reducerPath: 'goodsApi',
+	//для обновления после мутации
+	//конкретизация сущностей с которыми работаем в рамках этого API. то есть имя сущности для АПИ
+	tagTypes: ['Products'],
 	//базовый запрос
 	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001' }),
 	//поинты
@@ -10,10 +13,39 @@ export const goodsApi = createApi({
 		//query({передать обьект, с чем работаю})
 		getGoods: build.query({
 			//функция будет возвращать гудс - это дополнение к базовому адресу
-			query: () => `goods`,
+			query: (/*лимит, чтобы не все товарыф получать */ limit = '') =>
+				//проверка, есть ли лимит, если есть, то передаём строку с лимитом который передаём парамтером
+				//_limit служебн имя для лимита
+				//после запроса будет кэширование которые хранится несколько минут
+				`goods?${limit && `_limit=${limit}`}`,
+			//уточнение с чем работаем. ProvidesTag в гугле и сюда
+			providesTags: (result) =>
+				result
+					? [
+							...result.map(({ id }) => ({
+								type: 'Products',
+								id,
+							})), //все эл результата(goods) и каждому добавляем тип(Products)
+							{ type: 'Products', id: 'LIST' },
+					  ]
+					: [{ type: 'Products', id: 'LIST' }], //иначе пустой
+		}),
+		//новый поинт для добавления, для этого мутация нужна
+		addProduct: build.mutation({
+			query: (body) => ({
+				//с каким урл работаем
+				//запрост на саму сущность без уточнения
+				url: 'goods',
+				method: 'POST',
+				body,
+			}),
+			//уточнить с чем работал для синхрона после изменения ключ invalidatesTags
+			invalidatesTags: [{ type: 'Products', id: 'LIST' }], // то есть говорим что изменился список сущности, и тогда он заного запрос за продуктами делает
+			//не надо делать синхрон состояний, оно живёт у нас на сервере и перерисовывает. Сервер как источник истины
 		}),
 	}),
 });
-//сделать деструктр и вытащить хуки 
+//сделать деструктр и вытащить хуки
 //use хук GetGoods наш запрос Query (без мутации )
-export const {useGetGoodsQuery}=goodsApi
+//use хук AddProductMutation наш запрос mutation (с мутацией )
+export const { useGetGoodsQuery, useAddProductMutation } = goodsApi;
